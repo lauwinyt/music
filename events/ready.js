ea9fec0d35a96035f1f28d67db5f91a1a78a72ba
lauwinyt <yehudalau@gmail.com>
2024-08-05 17:41:12 +0300

    ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,45 +1,46 @@
-const config = require("../config.js");
-const { ActivityType } = require("discord.js");
+// events/ready.js
+const { REST, Routes, ActivityType } = require('discord.js'); // Import ActivityType
+const fs = require('fs');
+const status = "AUTOMATIC";
+const botName = "Music Bot [ Distube ]";
+const Text = "GlaceYT";
+const version = "Latest@ v4.0"; 
+const startTime = Date.now();
+const config = require('../config');
+
+function printWatermark() {
+  const uptimeInSeconds = ((Date.now() - startTime) / 1000).toFixed(2);
+
+  console.log('\x1b[1m\x1b[36m╔════════════════════════════════════════════╗');
+  console.log('\x1b[1m\x1b[36m║                                            ║');
+  console.log(`\x1b[1m\x1b[36m            ${botName}     `);
+  console.log(`\x1b[1m\x1b[36m            👑 Authorization : ${status}    `);
+  console.log(`\x1b[1m\x1b[36m            💡 Version: ${version}`);
+  console.log(`\x1b[1m\x1b[36m            📅 Uptime: ${uptimeInSeconds}s`);
+  console.log(`\x1b[1m\x1b[36m            🚀 Powered by ${Text}`);
+  console.log('\x1b[1m\x1b[36m║                                            ║');
+  console.log('\x1b[1m\x1b[36m╚════════════════════════════════════════════╝\x1b[0m');
+}
+printWatermark();
 
 module.exports = async (client) => {
+  console.log(`\x1b[31m[ CORE ]\x1b[0m \x1b[32mBot Name:  \x1b[0m${client.user.tag}`);
+  console.log(`\x1b[31m[ CORE ]\x1b[0m \x1b[32mClient ID: \x1b[0m${client.user.id}`);
+  
+  const commands = [];
+  fs.readdirSync('./commands').forEach(file => {
+    const command = require(`../commands/${file}`);
+    commands.push(command.data.toJSON());
+  });
 
-    if (config.mongodbURL || process.env.MONGO) {
+  const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+  await rest.put(
+    Routes.applicationCommands(client.user.id),
+    { body: commands },
+  );
 
-        const { REST } = require("@discordjs/rest");
-        const { Routes } = require("discord-api-types/v10");
-        const rest = new REST({ version: "10" }).setToken(config.TOKEN || process.env.TOKEN);
-        (async () => {
-            try {
-                await rest.put(Routes.applicationCommands(client.user.id), {
-                    body: await client.commands,
-                });
-                console.log('\x1b[36m%s\x1b[0m', '|    :rocket: Commands Loaded!')
-            } catch (err) {
-                console.log('\x1b[36m%s\x1b[0m', '|    :rocket: Commands Distracted!');
-            }
-        })();
-
-        console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
-
-        setInterval(() => {
-            const serverCount = client.guilds.cache.size;
-            client.user.setActivity({
-                name: `I am in ${serverCount} server`,
-                type: ActivityType.Watching
-            });
-        }, 10000);
-
-        client.errorLog = config.errorLog;
-    } else {
-        console.log('\x1b[36m%s\x1b[0m', `|    🍔 Error MongoDB!`)
-    }
-    console.log('\x1b[36m%s\x1b[0m', `|    🎯 Activity successfully set!`);
-
-    if (client.config.voteManager.status === true && client.config.voteManager.api_key) {
-        const { AutoPoster } = require('topgg-autoposter');
-        const ap = AutoPoster(client.config.voteManager.api_key, client);
-        ap.on('posted', () => {
-        });
-    }
-
-}
+  
+  client.user.setActivity('Netflix', {
+    type: ActivityType.Watching,
+  });
+};
